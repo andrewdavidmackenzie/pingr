584ffd2e86272d1e3a992c5034b89b329d80abcc
Andrew Mackenzie <andrew@mackenzie-serres.net>
2023-12-01 11:02:53 +0100

    Load default config and loop

--- wimon/src/main.rs
+++ wimon/src/main.rs
@@ -1,10 +1,33 @@
 use std::{env, io};
 use std::fmt::{Display, Formatter};
 use std::path::PathBuf;
+use std::time::Duration;
 use mac_address::get_mac_address;
+
+const CONFIG_FILE_NAME: &str = "wimon.toml";
 
 enum DeviceId {
     MAC([u8;6])
+}
+
+enum SSIDMonitorSpec {
+    /// Report status of all SSIDs that are detected at each monitoring moment
+    All,
+    /// Only report on the status of the SSID that is used to provide the internet connection
+    Connection,
+    /// Monitor a specific list of supplied SSIDs by name
+    SSIDList(Vec<String>)
+}
+
+impl Default for SSIDMonitorSpec {
+    fn default() -> Self {
+        SSIDMonitorSpec::Connection
+    }
+}
+
+#[derive(Default)]
+struct Config {
+    ssid_monitor_spec: SSIDMonitorSpec
 }
 
 impl Display for DeviceId {
@@ -14,14 +37,26 @@ impl Display for DeviceId {
         }
     }
 }
-const CONFIG_FILE_NAME: &str = "wimon.toml";
 
 fn main() -> Result<(), io::Error> {
     let config_file_path = find_config_file(CONFIG_FILE_NAME)?;
     println!("Config file found at {}", config_file_path.display());
+    let config = read_config(config_file_path)?;
 
     let device_id = get_device_id()?;
     println!("Device ID = {device_id}");
+
+    monitor_loop(config)?;
+
+    Ok(())
+}
+
+fn monitor_loop(_config: Config) -> Result<(), io::Error> {
+    loop {
+        std::thread::sleep(Duration::from_secs(10));
+
+        println!("Alive");
+    }
 
     Ok(())
 }
@@ -48,4 +83,8 @@ fn find_config_file(file_name: &str) -> Result<PathBuf, io::Error> {
 
     Err(io::Error::new(io::ErrorKind::NotFound,
                         "wimon toml config file not found"))
+}
+
+fn read_config(_config_file_path: PathBuf) -> Result<Config, io::Error> {
+    Ok(Config::default())
 }
