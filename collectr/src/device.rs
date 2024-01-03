ce0cd60c489f5b1d6b41c566e77a276f582f8ef5
Andrew Mackenzie <andrew@mackenzie-serres.net>
2024-01-03 15:17:29 +0100

    Implement Device state storage and retreival (async) in DO

--- collectr/src/device.rs
+++ collectr/src/device.rs
@@ -1,11 +1,12 @@
 use worker::*;
 use std::fmt::{Display, Formatter};
+use serde_derive::{Deserialize, Serialize};
 use worker::durable_object;
 use data_model::{DeviceId, MonitorReport, ReportType};
 use crate::device::DeviceState::{Offline, Reporting, NotReporting};
 // use data_model::MonitorReport;
 
-#[derive(Debug, PartialEq)]
+#[derive(Debug, PartialEq, Serialize, Deserialize, Clone)]
 enum DeviceState {
     /// The device stopped reporting, and is not considered offline
     NotReporting,
@@ -38,18 +39,18 @@ pub struct Device {
 #[durable_object]
 impl DurableObject for Device {
     fn new(state: State, env: Env) -> Self {
-        // Retrieve previous device_state. If not start in NotReporting
-        // How to load previous stateif this is NOT a new DO?
-//        let initial_state = state.storage().get("device_state").await.unwrap_or(NotReporting);
+        console_log!("DO - Creating new instance");
         Self {
             state,
             env,
-//            device_state: initial_state,
             device_state: NotReporting,
         }
     }
 
     async fn fetch(&mut self, mut req: Request) -> Result<Response> {
+        // Retrieve previous device_state. If not start in NotReporting
+        self.device_state = self.state.storage().get("device_state").await.unwrap_or(NotReporting);
+
         console_log!("DO request {} - Previous state: {}", req.path(), self.device_state);
 
         let form = req.form_data().await?;
@@ -58,26 +59,13 @@ impl DurableObject for Device {
                 FormEntry::Field(report_string) => {
                     let report_json: serde_json::Result<MonitorReport> = serde_json::from_str(&report_string);
                     match report_json {
-                        Ok(report) => {
-                            self.process_report(report, false);
-                            Response::ok("Report Processed")
-                        },
-                        Err(e) => {
-                            console_error!("Could not deserialize report: {e}");
-                            console_error!("{}", report_string);
-                            Response::error("Could not deserialize report", 400)
-                        }
+                        Ok(report) => self.process_report(report, false).await,
+                        Err(_) => Response::error("Could not deserialize report", 400)
                     }
                 },
-                _ => {
-                    console_error!("Unexpected File attached to report");
-                    Response::error("Unexpected File", 400)
-                }
+                _ => Response::error("Unexpected File attached to report", 400)
             }
-            _ => {
-                console_error!("Unexpected FormEntry");
-                Response::error("Unexpected FormEntry", 400)
-            }
+            _ => Response::error("Unexpected FormEntry in report FormData", 400)
         }
     }
 
@@ -87,19 +75,29 @@ impl DurableObject for Device {
 }
 
 impl Device {
-    fn new_state(&mut self, id: &DeviceId, new_state: DeviceState) {
-        self.device_state = new_state;
-        console_log!("Device ID: {} - Now in {} state", id, self.device_state);
+    // change the state of the tracked device to the new state, if it is different from the current state
+    // then store the state for use in future instances of this DurableObject
+    async fn new_state(&mut self, id: &DeviceId, new_state: DeviceState) -> Result<()> {
+        if self.device_state != new_state {
+            console_log!("Device ID: {} - State transition from {} to {}", id,
+                self.device_state, new_state);
+            self.device_state = new_state;
+            self.state.storage().put("device_state", &self.device_state).await
+        } else {
+            Ok(())
+        }
     }
 
-    fn process_report(&mut self, report: MonitorReport, alarm: bool) {
+    // Process a new report or alarm - implementing the state machine, changing to the new state when required
+    // and logging console warnings for states that should not happen if everything is working perfectly
+    async fn process_report(&mut self, report: MonitorReport, alarm: bool) -> Result<Response> {
         match (report.report_type, alarm) {
             (_, true) => {  // report overdue
                 match &self.device_state {
                     NotReporting => console_warn!("Report overdue with device in NotReporting state"),
                     Reporting => {
                         console_log!("Device ID: {} - Report overdue", report.device_id);
-                        self.new_state(&report.device_id, Offline);
+                        self.new_state(&report.device_id, Offline).await?;
                     },
                     Offline => console_warn!("Report overdue with device in Offline state"),
                 }
@@ -108,20 +106,22 @@ impl Device {
                 if self.device_state == Reporting {
                     console_warn!("Start Report with device in Reporting state");
                 }
-                self.new_state(&report.device_id, Reporting);
+                self.new_state(&report.device_id, Reporting).await?;
             },
             (ReportType::Stop, _) => { // Stop report
                 if self.device_state == NotReporting {
                     console_warn!("Stop Report with device in NotReporting state");
                 }
-                self.new_state(&report.device_id, NotReporting);
+                self.new_state(&report.device_id, NotReporting).await?;
             }
             (ReportType::OnGoing, _) => { // OnGoing report
                 if self.device_state == NotReporting {
                     console_warn!("OnGoing Report with device in NotReporting state");
                 }
-                self.new_state(&report.device_id, Reporting);
+                self.new_state(&report.device_id, Reporting).await?;
             }
         }
+
+        Response::ok("Report Processed")
     }
 }
--- collectr/src/lib.rs
+++ collectr/src/lib.rs
@@ -12,7 +12,7 @@ async fn main(req: Request, env: Env, _ctx: Context) -> Result<Response> {
                 Some(device_id) => {
                     let namespace = ctx.durable_object("DEVICES")?;
                     let stub = namespace.id_from_name(&device_id)?.get_stub()?;
-                    stub.fetch_with_request(req.clone().unwrap()).await
+                    stub.fetch_with_request(req).await
                 }
                 _ => Response::error("Bad Request - missing device id", 400),
             }
