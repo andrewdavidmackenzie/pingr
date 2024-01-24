20dac5357925dd22ba14dc4f2f413dd08064344c
Andrew Mackenzie <andrew@mackenzie-serres.net>
2024-01-24 11:16:58 +0100

    Add error hook

--- viewr/src/main.rs
+++ viewr/src/main.rs
@@ -1,5 +1,6 @@
 use leptos::*;
 
 fn main() {
-    mount_to_body(|| view! { <p>"Hello, world!"</p> })
+    console_error_panic_hook::set_once();
+    mount_to_body(|| view! { <h1>"Devices"</h1> })
 }
